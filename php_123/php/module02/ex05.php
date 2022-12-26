90226a1a7d464137b161d385992d5bafb2795fe2
Hatuanem0712 <119428520+Hatuanem0712@users.noreply.github.com>
2022-12-26 18:35:55 +0700

    123

---
+++ php_123/php/module02/ex05.php
@@ -0,0 +1,26 @@
+<?php
+$a = 1;
+$b = 2;
+$total = $a+$b+10;
+// echo $total;
+
+
+$age = 30;
+const _WEB_HOST_ROOT = 'https://vnexpress.net/';
+echo $age;
+echo '<br/>';
+echo _WEB_HOST_ROOT;
+echo '<br/>';
+
+
+$outputStr = '123';
+// $outputStr = $outputStr. '456';
+$outputStr.=' 456';
+$outputStr.=' 789<br/>';
+echo $outputStr;
+
+echo '<br/>';
+
+$check = $a>$b;
+var_dump($check);
+echo '<br/>';
