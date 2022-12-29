e2b8eb037309f3047a3686b20b52cb23b7f2692e
Hatuanem0712 <119428520+Hatuanem0712@users.noreply.github.com>
2022-12-29 12:59:38 +0700

    123

---
+++ php/loop/for/ex10.php
@@ -0,0 +1,56 @@
+<?php
+$n = 0;
+if (isset ( $_POST ['n'] )) {
+    $n = $_POST ['n'];
+}
+function phanTichSoNguyen($n) {
+    $i = 2;
+    $arrNumbers = array ();
+    $arrNumbers [0] = "";
+    $count = 0;
+ 
+    while ( $n > 1 ) {
+        if ($n % $i == 0) {
+            $n = floor ( $n / $i );
+            $arrNumbers [$count] = $i;
+            $count = $count + 1;
+        } else {
+            $i ++;
+        }
+    }
+
+    if ($arrNumbers [0] == "") {
+        $arrNumbers [0] = $n;
+    }
+    return $arrNumbers;
+}
+ 
+?>
+<form action="#" method="post">
+ <table>
+  <tr>
+   <td>Nhập số nguyên:</td>
+   <td><input type="text" name="n" value="<?=$n?>" /></td>
+  </tr>
+  <tr>
+   <td></td>
+   <td><input type="submit" value="Phân tích"></td>
+  </tr>
+ </table>
+</form>
+<br>
+<?php
+ 
+if (filter_var ( $n, FILTER_VALIDATE_INT )) {
+    $arrNumbers = phanTichSoNguyen ( $n );
+    echo ("Kết quả: $n = ");
+    $size = count ( $arrNumbers );
+ 
+    for($i = 0; $i < $size - 1; $i ++) {
+        echo "$arrNumbers[$i] x ";
+    }
+    echo $arrNumbers [$size - 1];
+} else {
+    echo ("Giá trị input không hợp lệ!");
+}
+?>
--- php/loop/for/ex7.php
+++ php/loop/for/ex7.php
@@ -1,28 +1,37 @@
 <?php
+
+echo 123;
+
 ?>
-<table width="100%" cellspacing="0" cellpadding="0" border="0">  
+<table width="100%" cellspacing="0" cellpadding="0" style="border: 1px solid red; height: 30px; 
+" >  
       
+
     <?php  
-    $alphaLoop = null;
-    for($row='a'; $row <= 'h'; $row++) {
-        $alphaLoop.='<td style="text-align: center;">'.$row.'</td>';
+    $alphaLoop = "";
+    
+    for($row=1; $row <= 8; $row++) {
+        $alphaLoop .='<td style="text-align: center;">'.$row.'</td>';
     }
-    echo '<tr style="height:30px;;><td></td>'.$alphaLoop.'</tr>';
+    echo '<tr style="height:30px;">'.$alphaLoop.'</tr>';
+
+
     $index = 8;
-    for ($row = 1; $row<=8; $row++){
+    for ($row1 = 1; $row1<=8; $row1++){
         echo '<tr>';
-        echo '<td width="30px">'.$index.'</td>';
-        for ($col = 1; $col<=8; $col++) {
-            $total = $row + $col;
-            if ($total % 2 == 0) {
-                echo '<td style="background: #fff; width: 12%; height: 100px;"></td>'
-            }else {
-                echo '<td style="background: #000; width: 12%; height: 100px;"></td>'
+
+        echo '<td style="text-align: center;">'.$index.'</td>';
+            for ($col = 1; $col<=7; $col++) {
+                    $total = $row1 + $col;
+                    if ($total % 2 == 0) {
+                        echo '<td style="background: #fff; width: 12%; height: 100px;">aaa</td>';
+                    }else {
+                        echo '<td style="background: #000; width: 12%; height: 100px;">bbbb</td>';
+                    }
             }
-        }
         echo '</tr>';
         $index--;
-    }   
+    }   1
     ?>  
 </table>
      
